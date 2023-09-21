44c18257018705d3920c2a2a2501ff5aa2ad35d2
YanaUSA <usatiukiana@gmail.com>
2023-09-21 13:16:16 +0300

    delete Login page logic as not necessary

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { config } from './lib/wagmi';
 
 import { Route, Routes } from 'react-router-dom';
 import Layout from '@components/Layout/Layout';
-// import LoginPage from '@pages/LoginPage/LoginPage';
 import HomePage from "@pages/HomePage/HomePage";
 import StakePage from '@pages/StakePage/StakePage';
 import WithdrawPage from '@pages/WithdrawPage/WithdrawPage';
@@ -18,7 +17,6 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<HomePage />} />
-                    {/* <Route index element={<LoginPage />} /> */}
                     <Route path="/stake" element={<StakePage />} />
                     <Route path="/withdraw" element={<WithdrawPage />} />
                     <Route path="/rewards" element={<RewardsPage />} />
